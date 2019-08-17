7f4cb35b0c858e0f172e0b39ca864f2bd1125920
Alexander Johnston <alexanderj.roaming@gmail.com>
2019-08-16 21:10:25 -0400

    Mock some Totem-Timeline-js Flow functionality.

---
+++ BlazorUI.Client/totem-timeline/Event.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Totem;
+using Totem.Runtime;
+
+namespace BlazorUI.Client.totem_timeline
+{
+    public class Event : IBindable
+    {
+        protected Event()
+        {
+            When = Clock.Now;
+        }
+
+        public Fields Fields => _fields ?? (_fields = new Fields(this));
+        private Fields _fields;
+        Fields IBindable.Fields => Fields;
+
+        protected IClock Clock => Traits.Clock.Get(this);
+
+        public DateTimeOffset When
+        {
+            get { return Traits.When.Get(this); }
+            private set { Traits.When.Set(this, value); }
+        }
+
+        public Field Position
+        {
+            get { return Traits.Position; }
+            set { Traits.Position.Set(this, value);  }
+        }
+
+        public Field Type
+        {
+            get { return Traits.Type; }
+            set { Traits.Type.Set(this, value); }
+        }
+
+        public Field Cause
+        {
+            get { return Traits.Cause; }
+            set { Traits.Cause.Set(this, value); }
+        }
+
+        public Field WhenOccurs
+        {
+            get { return Traits.WhenOccurs; }
+            set { Traits.WhenOccurs.Set(this, value); }
+        }
+        public static DateTimeOffset? GetWhenOccurs(Event e) =>
+            Traits.WhenOccurs.Get(e);
+
+        public static bool IsScheduled(Event e) =>
+          GetWhenOccurs(e) != null;
+
+        public new static class Traits
+        {
+            public static readonly Field<string> Cause = Field.Declare(() => Cause);
+            public static readonly Field<string> Type = Field.Declare(() => Type);
+            public static readonly Field<int> Position = Field.Declare(() => Position);
+            public static readonly Field<DateTimeOffset> When = Field.Declare(() => When);
+            public static readonly Field<DateTimeOffset?> WhenOccurs = Field.Declare(() => WhenOccurs);
+            public static readonly Field<Id> EventId = Field.Declare(() => EventId);
+            public static readonly Field<Id> CommandId = Field.Declare(() => CommandId);
+            public static readonly Field<Id> UserId = Field.Declare(() => UserId);
+            public static readonly Field<IClock> Clock = Field.Declare(() => Clock, new PlatformClock());
+            class PlatformClock : IClock
+            {
+                public DateTimeOffset Now => DateTimeOffset.Now;
+            }
+        }
+
+        
+
+    }
+}
--- BlazorUI.Client/totem-timeline/Flows.cs
+++ BlazorUI.Client/totem-timeline/Flows.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Reactive;
 using System.Linq;
 using System.Threading.Tasks;
+using Totem.Timeline;
 
 namespace BlazorUI.Client.totem_timeline
 {
     public class Flows
     {
         public Dictionary<dynamic, dynamic> EventTypes { get; set; }
+        
+        public IObserver<dynamic> Events { get; set; }
+
+        private int _position = 0;
+
+        public void AppendEvent(string cause, string type, Event data)
+        {
+            _position++;
+            data.Fields.Set(data.Position, _position);
+            data.Fields.Set(data.Cause, cause);
+            data.Fields.Set(data.Type, type);
+            Events.OnNext(data);
+        }
+
+        public void ScheduleEvent(DateTime whenOccurs, string cause, string type, Event data)
+        {
+            _position++;
+            data.Fields.Set(data.Position, _position);
+            data.Fields.Set(data.Cause, cause);
+            data.Fields.Set(data.Type, type);
+            data.Fields.Set(data.WhenOccurs, whenOccurs);
+            Events.OnNext(data);
+        }
+
+        public void AppendScheduledEvent(Event data)
+        {
+            _position++;
+            data.Fields.Set(data.Position, _position);
+            data.Fields.Set(data.WhenOccurs, null);
+            Events.OnNext(data);
+        }
+
+
     }
 }
