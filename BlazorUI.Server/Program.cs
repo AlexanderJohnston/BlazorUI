571ae3f6e668178e2ad34abb02c3dab794264220
Alexander Johnston <alexanderj.roaming@gmail.com>
2019-08-29 20:24:09 -0400

    Add BlazorUI Client-side Files Server Startup

--- BlazorUI.Server/Program.cs
+++ BlazorUI.Server/Program.cs
@@ -13,6 +13,8 @@ using Microsoft.AspNetCore.Components;
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using BlazorUI.Client;
+using Blazor.Extensions;
 
 namespace BlazorUI.Server
 {
@@ -20,36 +22,43 @@ namespace BlazorUI.Server
     {
         public static Task Main()
         {
-            Type type = MethodBase.GetCurrentMethod().DeclaringType;
-            /*WebApp.Run<CamArea>(Configure.App(configure => 
-            { 
-                configure.UseClientSideBlazorFiles<BlazorUI.Client.Startup>(); 
-            }));*/
+            // old method, working method
+            //Type type = MethodBase.GetCurrentMethod().DeclaringType;
             //return WebApp.Run<CamArea>(Assembly.GetAssembly(type));
-            return WebApp.Run<CamArea>((configure, services) => {
-                services.AddServerSideBlazor();
-                // Server side Blazor doesn't provide HttpClient by default
-                services.AddScoped<HttpClient>(s =>
+            return WebApp.Run<CamArea>(Configure
+                .App(configure =>
                 {
-                    // Creating the URI helper needs to wait nutil JS Runtime is initialized, so defer it
-                    var uriHelper = s.GetRequiredService<IUriHelper>();
-                    return new HttpClient
+                    configure.UseClientSideBlazorFiles<BlazorUI.Client.Startup>();
+                })
+                .Services((context, services) =>
+                {
+                    services.AddServerSideBlazor();
+                    // Server side Blazor doesn't provide HttpClient by default
+                    services.AddScoped<HttpClient>(s =>
                     {
-                        BaseAddress = new Uri(uriHelper.GetBaseUri())
-                    };
-                });
-                services.AddHttpClient();
-                services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
-                .AddRazorRuntimeCompilation()
-                .AddNewtonsoftJson();
-                services.AddResponseCompression(opts =>
-                {
-                    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
-                        new[] { "application/octet-stream" });
-                });
-            });
-
+                        // Creating the URI helper needs to wait nutil JS Runtime is initialized, so defer it
+                        var uriHelper = s.GetRequiredService<IUriHelper>();
+                        return new HttpClient
+                        {
+                            BaseAddress = new Uri(uriHelper.GetBaseUri())
+                        };
+                    });
+                    services.AddHttpClient();
+                    services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                    .AddRazorRuntimeCompilation()
+                    .AddNewtonsoftJson();
+                    services.AddResponseCompression(opts =>
+                    {
+                        opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                            new[] { "application/octet-stream" });
+                    });
+                    // Register client dependecies.
+                    services.AddTransient<HubConnectionBuilder>();
+                    services.AddSingleton<QueryController>();
+                    services.AddSingleton<AppState>();
+                })
+            );
         }
     }
 
