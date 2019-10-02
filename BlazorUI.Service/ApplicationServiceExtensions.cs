debeeef518d9eb88d698c10b8b99c69c5553f16e
Alexander Johnston <alexanderj.roaming@gmail.com>
2019-10-01 20:29:05 -0400

    Remove unused code from AppExtensions

--- BlazorUI.Service/ApplicationServiceExtensions.cs
+++ BlazorUI.Service/ApplicationServiceExtensions.cs
@@ -44,35 +44,5 @@ namespace BlazorUI.Service
         {
             return services.AddSingleton<ILegacyEventContext, DatabaseService>(s => new DatabaseService(connection));
         }
-
-        /*
-        static IServiceCollection AddDealerOnDb(this IServiceCollection services) =>
-          services.AddSingleton<IDealerOnDb>(s => new DealerOnDb(s.GetOptions<ApplicationOptions>().DealerOnConnectionString));
-
-        static IServiceCollection AddManifests(this IServiceCollection services) =>
-          services
-          .AddSingleton<IManifestFile>(s => new ManifestFile(
-            HttpLink.From(s.GetOptions<CamOptions>().ManifestLink),
-            s.GetRequiredService<IHttpClientFactory>(),
-            s.GetRequiredService<IEligibleDealerDb>()))
-          .AddSingleton<IEligibleDealerDb, EligibleDealerDb>()
-          .AddSingleton<IDealerManifestFile, DealerManifestFile>();
-
-        static IServiceCollection AddAssets(this IServiceCollection services) =>
-          services
-          .AddSingleton<IAssetFile, AssetFile>()
-          .AddSingleton<IAssetFolder>(s => new AssetFolder(s.GetOptions<CamOptions>().AssetFolder))
-          .AddSingleton<IAssetDb, AssetDb>();
-
-        static IServiceCollection AddCampaigns(this IServiceCollection services) =>
-          services
-          .AddSingleton<IBannerPath>(s => new BannerPath(s.GetOptions<CamOptions>().BannerPath))
-          .AddSingleton<HomeBannerTable>()
-          .AddSingleton<ConditionalBannerTable>()
-          .AddSingleton<ICampaignDb, CampaignDb>(s => new CampaignDb(
-            s.GetRequiredService<IDealerOnDb>(),
-            s.GetRequiredService<HomeBannerTable>(),
-            s.GetRequiredService<ConditionalBannerTable>()));
-            */
     }
 }
