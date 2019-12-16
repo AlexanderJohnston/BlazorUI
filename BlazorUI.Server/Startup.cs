ce6a7a3953cbf6d963e0fd182692d69d633547d8
Alexander Johnston <alexanderj.roaming@gmail.com>
2019-12-16 00:01:18 -0500

    Comment out unnecessary Startup file for now.

--- BlazorUI.Server/Startup.cs
+++ BlazorUI.Server/Startup.cs
@@ -7,42 +7,42 @@ using System.Linq;
 
 namespace BlazorUI.Server
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().AddNewtonsoftJson();
-            services.AddResponseCompression(opts =>
-            {
-                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
-                    new[] { "application/octet-stream" });
-            });
-        }
-        public bool NotHomenet(string id) => id != "homenet";
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            var feeds = new string[] { "homenet", "cpo", "chrome" };
-            if (feeds.All(f => NotHomenet(f)))
-                app.UseResponseCompression();
+    //public class Startup
+    //{
+    //    // This method gets called by the runtime. Use this method to add services to the container.
+    //    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    //    public void ConfigureServices(IServiceCollection services)
+    //    {
+    //        services.AddMvc().AddNewtonsoftJson();
+    //        services.AddResponseCompression(opts =>
+    //        {
+    //            opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+    //                new[] { "application/octet-stream" });
+    //        });
+    //    }
+    //    public bool NotHomenet(string id) => id != "homenet";
+    //    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    //    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    //    {
+    //        var feeds = new string[] { "homenet", "cpo", "chrome" };
+    //        if (feeds.All(f => NotHomenet(f)))
+    //            app.UseResponseCompression();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBlazorDebugging();
-            }
+    //        if (env.IsDevelopment())
+    //        {
+    //            app.UseDeveloperExceptionPage();
+    //            app.UseBlazorDebugging();
+    //        }
 
-            app.UseClientSideBlazorFiles<Client.Startup>();
+    //        app.UseClientSideBlazorFiles<Client.Startup>();
 
-            app.UseRouting();
+    //        app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-                endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
-            });
-        }
-    }
+    //        app.UseEndpoints(endpoints =>
+    //        {
+    //            endpoints.MapDefaultControllerRoute();
+    //            endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
+    //        });
+    //    }
+    //}
 }
